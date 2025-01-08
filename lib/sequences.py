359fb80cf38f46ba25025a7ca913602e3ca02d90
enock-jufri <enockjufri@gmail.com>
2025-01-08 11:41:18 +0300

    fibonnaci in python

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if(length==0):
+        print([])
+    elif(length==1):
+        print([0])
+    else:
+        fib=[0,1]
+        for i in range(length-2):
+            fib.append(fib[-1] + fib[-2])
+        print(fib)
+print_fibonacci(0)
